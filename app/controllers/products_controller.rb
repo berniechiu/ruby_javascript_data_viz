3d457be2cbeb038808ca735dc206a4431709e66c
Bernie Chiu <bernie_chiu@hotmail.com>
2013-11-17 15:17:03 +0800

    Generate products chart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,22 @@
 class ProductsController < ApplicationController
 
 	def index
-		@products = Product.includes(:user).all
+		@products = Product.includes(:user).first(20)
+    @sum_price = Product.sum(:price).to_f
 	end
 
 	def create
 		@product = Product.new(params[:product])
 
 		respond_to do |format|
-  		if @product.save
-   		 format.html { render :action => "create" }
-   		 format.json { render :json => @product }
- 			else
-    		 format.html { render :action => "new" }
-  		 format.json { render :json => @product.errors, :status => :unprocessable_entity }
- 			end
-		end  
+      if @product.save
+        format.html { render :action => "create" }
+        format.json { render :json => @product }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @product.errors, :status => :unprocessable_entity }
+      end
+    end
 	end
 
 	def new
